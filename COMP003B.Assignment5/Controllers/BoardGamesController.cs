3e2d0af39ad7d52aa20a2e20d1f1b822f2b5a873
gonzalez-7 <20agonzalezp@gmail.com>
2025-04-23 23:10:58 -0700

    Created the API Controller named BoardGamesController.cs

---
+++ COMP003B.Assignment5/Controllers/BoardGamesController.cs
@@ -0,0 +1,54 @@
+﻿using COMP003B.Assignment5.Models;
+using COMP003B.Assignment5.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace COMP003B.Assignment5.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class BoardGamesController : ControllerBase
+	{
+		[HttpGet]
+		public ActionResult<List<BoardGame>> Get() => GameStore.Games;
+
+		[HttpGet("{id}")]
+		public ActionResult<BoardGame> Get(int id)
+		{
+			var game = GameStore.Games.FirstOrDefault(g => g.Id == id);
+			return game == null ? NotFound() : Ok(game);
+		}
+
+		[HttpPost]
+		public ActionResult<BoardGame> Post(BoardGame newGame)
+		{
+			newGame.Id = GameStore.Games.Max(g => g.Id) + 1;
+			GameStore.Games.Add(newGame);
+			return CreatedAtAction(nameof(Get), new { id = newGame.Id }, newGame);
+		}
+
+		[HttpPut("{id}")]
+		public IActionResult Put(int id, BoardGame updatedGame)
+		{
+			var game = GameStore.Games.FirstOrDefault(g => g.Id == id);
+			if (game == null) return NotFound();
+			if (id != updatedGame.Id) return BadRequest();
+
+			game.Title = updatedGame.Title;
+			game.MinPlayers = updatedGame.MinPlayers;
+			game.MaxPlayers = updatedGame.MaxPlayers;
+			game.Rating = updatedGame.Rating;
+
+			return NoContent();
+		}
+
+		[HttpDelete("{id}")]
+		public IActionResult Delete(int id)
+		{
+			var game = GameStore.Games.FirstOrDefault(g => g.Id == id);
+			if (game == null) return NotFound();
+			GameStore.Games.Remove(game);
+			return NoContent();
+		}
+	}
+}
