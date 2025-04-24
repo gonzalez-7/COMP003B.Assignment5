9613bc0356ca03f2255a9f9914f9347046055531
gonzalez-7 <20agonzalezp@gmail.com>
2025-04-23 22:52:14 -0700

    Created a new folder named Models and added a class inside of it named BoardGame.cs

---
+++ COMP003B.Assignment5/Models/BoardGame.cs
@@ -0,0 +1,11 @@
+﻿namespace COMP003B.Assignment5.Models
+{
+	public class BoardGame
+	{
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public int MinPlayers { get; set; }
+		public int MaxPlayers { get; set; }
+		public decimal Rating { get; set; }
+	}
+}
