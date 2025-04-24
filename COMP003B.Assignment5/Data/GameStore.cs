59d28a54d10c6802801d0662801900843dc3ba69
gonzalez-7 <20agonzalezp@gmail.com>
2025-04-23 23:03:19 -0700

    Added a new folder named Data and added a class named GameStore.cs for the In-Memory Data Store

---
+++ COMP003B.Assignment5/Data/GameStore.cs
@@ -0,0 +1,14 @@
+﻿using COMP003B.Assignment5.Models;
+
+namespace COMP003B.Assignment5.Data
+{
+	public static class GameStore
+	{
+		public static List<BoardGame> Games { get; } = new List<BoardGame>
+		{
+			new BoardGame { Id = 1, Title = "Catan", MinPlayers = 3, MaxPlayers = 4, Rating = 4.5M },
+			new BoardGame { Id = 2, Title = "Carcassonne", MinPlayers = 2, MaxPlayers = 5, Rating = 4.2M },
+			new BoardGame { Id = 3, Title = "Ticket to Ride", MinPlayers = 2, MaxPlayers = 5, Rating = 4.7M }
+		};
+	}
+}
